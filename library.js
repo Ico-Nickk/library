e2df15cb9b759ae1ede5b974dd0fc7af686cc96e
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-02-26 14:21:48 +0300

    created function to add book to library array

--- library.js
+++ library.js
@@ -7,6 +7,21 @@ function Book(title, author, pages, read) {
     this.read = read,
     this.id = crypto.randomUUID()
 }
- 
-let shakespear = new Book("shakespear", "shakespear", "245", "not read")
+
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
+
+addBookToLibrary("chicken", "fish", "24", "read");
+addBookToLibrary("tom and jerry", "mr bean", "400", "read");
+
+function createCard() {
+    const newdiv = document.createElement("div");
+    newdiv.className = "bookCard";
+    document.body.appendChild(newdiv);
+}
+
+createCard();
 
