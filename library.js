f40141353682f181504653ea392492c2e8a813b3
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-02-26 14:03:36 +0300

    added ID property to book constructor

--- library.js
+++ library.js
@@ -4,6 +4,9 @@ function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.read = read
+    this.read = read,
+    this.id = crypto.randomUUID()
 }
+ 
+let shakespear = new Book("shakespear", "shakespear", "245", "not read")
 
