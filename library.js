772f63e76315fbfe51e1aae067257c2d85b63167
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-02-26 14:36:40 +0300

    created functions to generate a new div for each book and a fucntion to loop through the library and display all books

--- library.js
+++ library.js
@@ -17,11 +17,22 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary("chicken", "fish", "24", "read");
 addBookToLibrary("tom and jerry", "mr bean", "400", "read");
 
-function createCard() {
+const mobbieDick = new Book("mobbie dick", "author", "300", "read")
+
+function createCard(book) {
     const newdiv = document.createElement("div");
     newdiv.className = "bookCard";
+    const newParagraph = document.createElement("p")
+    newParagraph.textContent = `${book.title} by ${book.author} has ${book.pages} pages. has been ${book.read}`;
+    newdiv.appendChild(newParagraph);
     document.body.appendChild(newdiv);
 }
 
-createCard();
+function displayBooks() {
+    for(book in myLibrary) {
+        createCard(book);
+    };
+}
+
+displayBooks()
 
