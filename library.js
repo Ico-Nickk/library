86c7fd2cee5d39407247f05e679f583fe2068149
Nicholas <205763400+Ico-Nickk@users.noreply.github.com>
2026-03-02 09:56:01 +0300

    final code.

--- library.js
+++ library.js
@@ -6,26 +6,47 @@ function Book(book) {
     this.pages = book.pages,
     this.read = book.read,
     this.id = crypto.randomUUID()
-}
+};
+
+Book.prototype.readStatus = function (){
+    if (this.read === "read") {
+        this.read = "not read";
+    } else {
+        this.read = "read"
+    };
+};
 
 function addBookToLibrary(book) {
     let newBook = new Book(book);
     myLibrary.push(newBook);
     createCard(newBook)
-}
+};
+
 const cardDisplay = document.querySelector(".cardDisplay");
 
 function createCard(book) {
     const newdiv = document.createElement("div");
     newdiv.className = "bookCard";
     newdiv.dataset.bookId = book.id;
+
     const newParagraph = document.createElement("p");
+    newParagraph.className = "cardPara"
     newParagraph.textContent = `${book.title} by ${book.author} has ${book.pages} pages. has been ${book.read}`;
     newdiv.appendChild(newParagraph);
+
     const removeBttn = document.createElement("button");
     removeBttn.textContent = "remove book"
     removeBttn.className = "removeBttn"
     newdiv.appendChild(removeBttn);
+
+    const readStatus = document.createElement("button");
+    if(book.read === "read") {
+        readStatus.textContent = "not read";
+    } else {
+        readStatus.textContent = "read";
+    };
+    readStatus.className = "readStatus";
+    newdiv.appendChild(readStatus);
     cardDisplay.appendChild(newdiv);
 }
 
@@ -53,8 +74,27 @@ cardDisplay.addEventListener('click', (event) => {
         alert(`book of id ${idOfBookToRemove} has been removed`);
         clickedDiv.remove();
     };
+
 });
 
+cardDisplay.addEventListener('click', (event) => {
+    const clickedDiv = event.target.parentNode;
+    const idOfBookTochangeReadStatus = clickedDiv.dataset.bookId;
+    const indexOfBook = myLibrary.findIndex(item => item.id === idOfBookTochangeReadStatus);
+    const cardpara = clickedDiv.querySelector(".cardPara");
+    if(event.target.className === "readStatus") {
+        if(event.target.textContent === "read") {
+            event.target.textContent = "not read"
+            myLibrary[indexOfBook].readStatus();
+            cardpara.textContent = `${myLibrary[indexOfBook].title} by ${myLibrary[indexOfBook].author} has ${myLibrary[indexOfBook].pages} pages. has been ${myLibrary[indexOfBook].read}`;
+        } else {
+            event.target.textContent = "read"
+            myLibrary[indexOfBook].readStatus();
+            cardpara.textContent = `${myLibrary[indexOfBook].title} by ${myLibrary[indexOfBook].author} has ${myLibrary[indexOfBook].pages} pages. has been ${myLibrary[indexOfBook].read}`;
+        }
+    };
+    
+})
 
 
 
